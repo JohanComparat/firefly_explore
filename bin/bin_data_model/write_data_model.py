4dbc3dd9c7ecb3c1fc3825be843ee868909c90d5
JohanComparat <johan.comparat@gmail.com>
2018-05-31 10:28:56 +0200

    data model

--- bin/bin_data_model/write_data_model.py
+++ bin/bin_data_model/write_data_model.py
@@ -6,18 +6,18 @@ from lib_spm import *
 
 f=open('../../data/data_model/sdss.tex', 'w')
 for el in sdss.columns :
-	f.write('\\item '+r"\_".join(el.name.split('_'))+": \n")
+	f.write(r"\_".join(el.name.split('_'))+":  \\newline \n")
 
 f.close()
 
 f=open('../../data/data_model/deep2.tex', 'w')
 for el in deep2.columns :
-	f.write('\\item '+r"\_".join(el.name.split('_'))+": \n")
+	f.write(r"\_".join(el.name.split('_'))+":  \\newline \n")
 
 f.close()
 
 f=open('../../data/data_model/eboss.tex', 'w')
 for el in boss.columns :
-	f.write('\\item '+r"\_".join(el.name.split('_'))+": \n")
+	f.write(r"\_".join(el.name.split('_'))+":  \\newline \n")
 
 f.close()
