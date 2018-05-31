51bb134d8e211a00abb89adb2da175b590f2f15f
JohanComparat <johan.comparat@gmail.com>
2018-05-31 09:44:40 +0200

    data model latex script

--- bin/bin_data_model/write_data_model.py
+++ bin/bin_data_model/write_data_model.py
@@ -4,6 +4,20 @@ from lib_spm import *
 #sdss
 #deep2
 
-f=open('../data/data_model/sdss.tex', 'w')
-for el in :
-	f.write('\\item '++": \n")
+f=open('../../data/data_model/sdss.tex', 'w')
+for el in sdss.columns :
+	f.write('\\item '+r"\_".join(el.name.split('_'))+": \n")
+
+f.close()
+
+f=open('../../data/data_model/deep2.tex', 'w')
+for el in deep2.columns :
+	f.write('\\item '+r"\_".join(el.name.split('_'))+": \n")
+
+f.close()
+
+f=open('../../data/data_model/eboss.tex', 'w')
+for el in boss.columns :
+	f.write('\\item '+r"\_".join(el.name.split('_'))+": \n")
+
+f.close()
