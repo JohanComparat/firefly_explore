16a08796bb3bfa9752e246bb6f987a4079e4748f
JohanComparat <johan.comparat@gmail.com>
2019-01-24 10:52:39 +0100

    sigma M

--- bin/run_analysis.py
+++ bin/run_analysis.py
@@ -67,8 +67,11 @@ python3.4 plot_differences_age_deep2.py
 # Figure 7
 python3.4 plot_dust_mass_plane.py
 
+# Figure 11, NZ
+
 # Figure 14 
 python3.4 plot_SNMED_uncertainty.py
+
 
 # Figure 1 
 # first row
